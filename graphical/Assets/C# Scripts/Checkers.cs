1785d00dc698e0b5c4be0e4a011dd3177effc475
Aziz Chigri <aziz.chigri@epitech.eu>
2018-06-11 18:04:31 +0200

    Add Board

---
+++ graphical/Assets/C# Scripts/Checkers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkers : MonoBehaviour {
+
+    public Board board;
+
+	// Use this for initialization
+	void Start () {
+        GameObject boardObject = new GameObject("Board");
+        boardObject.transform.parent = transform;
+        board = boardObject.AddComponent<Board>();
+        board.tilePrefab = (GameObject) Resources.Load("Prefabs/Tile");
+	}
+	
+}
