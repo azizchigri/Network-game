bfda34343fddee98a91a910f7d160aea36d7f246
Thibaud <thibaud.salsa@epitech.eu>
2018-06-24 21:24:53 +0200

    [FIXE] norm + test criterion failing

--- server/src/parameters.c
+++ server/src/parameters.c
@@ -36,26 +36,17 @@ int get_int_parameter(char *param)
 int manage_parameters(int ac, char **av, int c, t_server *server)
 {
 	switch (c) {
-		case 'p':
-			server->options.port = get_int_parameter(optarg);
-			break;
-		case 'x':
-			server->options.width = get_int_parameter(optarg);
-			break;
-		case 'y':
-			server->options.height = get_int_parameter(optarg);
-			break;
-		case 'n':
-			get_n_parameter(ac, av, optind,	server);
-			break;
-		case 'c':
-			server->options.nb_clients = get_int_parameter(optarg);
-			break;
-		case 'f':
-			server->options.frequence = get_int_parameter(optarg);
-			break;
-		default:
-			return (ERROR);
+	case 'p':
+		server->options.port = get_int_parameter(optarg);
+		break;
+	case 'x':
+		server->options.width = get_int_parameter(optarg);
+		break;
+	case 'y':
+		server->options.height = get_int_parameter(optarg);
+		break;
+	default:
+		return (manage_parameters_cases(ac, av, c, server));
 	}
 	return (0);
 }
