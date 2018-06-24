07caa174f59014881b169f8fb93d05fbe73a1dd5
Robin Chatain <robin.chatain@epitech.eu>
2018-06-24 17:15:17 +0200

    [Add] zappy_manual for testing purposes

---
+++ client/tests/zappy_manual/include/client.h
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2018
+** zappy
+** File description:
+** Client's header program
+*/
+
+#ifndef CLIENT_H_
+	#define CLIENT_H_
+
+	#include <unistd.h>
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netdb.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <errno.h>
+	#include <arpa/inet.h>
+	#include <string.h>
+	#include <getopt.h>
+	#include "get_next_line.h"
+
+//communication
+	//Management for "-help" argument
+	int get_help(int argc, char **argv);
+	//Print usage program when error or "-help" argument found
+	void print_usage();
+	//get arguments given on program's launch
+	char **get_args(int argc, char **argv);
+	//Check if necessary arguments are here
+	int check_args(char **args);
+	//launch main program
+	int launch_client(char **args);
+	//initialize a file descriptor for socket
+	int init_socket();
+	//Connect the program on server
+	int init_connection(char **av, int fd);
+	//Management in case of error when socket is open
+	int return_error(int fd);
+	//(Client_manual) Direct communication between client and server
+	int client_actions(int fd, char *name);
+	//Close the socket
+	int stop_socket(int fd);
+
+#endif /* !CLIENT_H_ */
