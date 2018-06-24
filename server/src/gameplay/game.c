97b14a50d1656b7865f517cb4f57a9144f4ec202
bchadelaud <bchadelaud>
2018-06-24 15:39:03 +0200

    [ADD] Broadcast on server

--- server/src/gameplay/game.c
+++ server/src/gameplay/game.c
@@ -10,7 +10,7 @@
 char *check_win(t_game_p *game)
 {
 	int i;
-	char *respond = "KO";
+	char *respond = "ko";
 	for (i = 0; game->teams[i] != NULL; i += 1) {
 		if (game->teams[i]->lvl6 >= 6)
 			respond = game->teams[i]->name;
--- server/src/reseau/execute.c
+++ server/src/reseau/execute.c
@@ -39,22 +39,67 @@ void eat_client(t_server *server)
 		server->eat -= 1;
 }
 
+void check_client_win(t_server *server)
+{
+	char *status = check_win(server->game);
+	if (strcmp(status, "ko") != 0) {
+		t_client *client = server->client;
+		printf("Le gagnant est:%s\n", status);
+		for (; client != NULL; client = client->next) {
+			destroy_client(server, client);
+		}
+		//free server
+		exit(0);
+	}
+}
+
+int execute_other_cmd(t_server *server, t_client *client, char **tab)
+{
+	if (tab != NULL && strcmp(tab[0], "TEAM") == 0) {
+		connect_client(server, client, client->fd, tab);
+		return (0);
+	} else if (tab != NULL && strcmp(tab[0], "Graphical") == 0) {
+		server->graph = client->fd;
+		send(client->fd, "ok\n", 3, 0);
+		clear_cmd(client);
+		//msz et mct
+		return (0);
+	}
+	if (tab != NULL && strcmp(tab[0], "Broadcast") == 0 &&
+	    client->buf[0].time == 0) {
+		t_client *tmp = server->client;
+		char *msg;
+		int size = 0;
+		for (; tmp != NULL; tmp = tmp->next) {
+			if (tmp != client) {
+				msg = broadcast(server->game, client->player,
+				tmp->player);
+				size = strlen(msg);
+				msg = realloc(msg, size + 2);
+				strcat(msg, "\n\0");
+				send(tmp->fd, msg, size, 0);
+				free(msg);
+			}
+		}
+		send(client->fd, "ok\n", 3, 0);
+		clear_cmd(client);
+	}
+	return (-1);
+}
+
 int execute_commands(t_server *server)
 {
 	t_client *client = server->client;
+	int result;
 	while (client != NULL) {
 		char **tab = client->buf[0].cmd;
-		if (tab != NULL && strcmp(tab[0], "TEAM") == 0) {
-			connect_client(server, client, client->fd, tab);
-		} else if (tab != NULL && strcmp(tab[0], "Graphical") == 0) {
-			server->graph = client->fd;
-			send(client->fd, "ok\n", 3, 0);
-			clear_cmd(client);
-			printf("hey le client graphique est:%d\n",server->graph);
-		} else if (client->buf[0].time != -1)
+		result = execute_other_cmd(server, client, tab);
+		if (result == -1 && (client->buf[0].time != -1)) {
 			manage_cmd(server, client);
+		}
 		client = client->next;
 	}
 	eat_client(server);
+	check_client_win(server);
 	return (0);
 }
